7bf22d434596662da5196fa9f7a4dcd2fc29a9b1
Ahmed Mohamed Kamel Mohamed <a.mohamed@zedia-g.jp>
2020-12-28 14:45:30 +0900

    updating week 2 of SQL for datascience

--- Coursera/IBM Data Analyst Professional Certificate/Databases and SQL for Data Science/week 2/Practice Aggregate, Scalar, String, Date and Time Functions.sql
+++ Coursera/IBM Data Analyst Professional Certificate/Databases and SQL for Data Science/week 2/Practice Aggregate, Scalar, String, Date and Time Functions.sql
@@ -49,17 +49,33 @@ where Animal='Dog' ;
 -- Exercise 3: Scalar and String Functions
 
 --Query B1: Enter a function that displays the rounded cost of each rescue.
-select rounding(COST/QUANTITY) from PETRESCUE;
+select round(COST) from PETRESCUE;
 
 --Query B2: Enter a function that displays the length of each animal name.
 select length(Animal) from PETRESCUE;
 
 --Query B3: Enter a function that displays the animal name in each rescue in uppercase.
-
+select UCASE(ANIMAL) from PETRESCUE;
 
 --Query B4: Enter a function that displays the animal name in each rescue in uppercase without duplications.
-select upper(distinct Animal) from PETRESCUE;
+select DISTINCT(UCASE(ANIMAL)) from PETRESCUE;
 
 --Query B5: Enter a query that displays all the columns from the PETRESCUE table, where the animal(s) rescued are cats. Use cat in lower case in the query.
 select * from PETRESCUE where ANIMAL = 'Cat';
+
+--Exercise 4: Date and Time Functions
+--Query C1: Enter a function that displays the day of the month when cats have been rescued.
+select extract(day from date(RESCUEDATE)) from PETRESCUE;
+
+
+--Query C2: Enter a function that displays the number of rescues on the 5th day of the month.
+select count(*), date(RESCUEDATE) from PETRESCUE where extract(day from RESCUEDATE) = '05' or extract(day from date(RESCUEDATE)) = 5;
+
+
+--Query C3: Animals rescued should see the vet within three days of arrivals. Enter a function that displays the third day from each rescue.
+select DATEADD(day, 3, RESCUEDATE) from PETRESCUE;
+
+--Query C4: Enter a function that displays the length of time the animals have been rescued; the difference between today’s date and the rescue date.
+select (CURRENT DATE - RESCUEDATE) from PETRESCUE;
+
 
